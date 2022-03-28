2ca7c88560f506a4a6dea8051c983717a4147d4b
Arina <arinakazakovak@gmail.com>
2022-03-28 13:39:51 +0300

    fix types and brands values,  update specializations select

--- src/apis/filters/by-specializations.controller.ts
+++ src/apis/filters/by-specializations.controller.ts
@@ -20,15 +20,15 @@ export default async (req: Request, res: Response): Promise<Response> => {
     }
 
     const query = `
-        SELECT e."id", e."email", e."firstName", e."zip", b."name" as "brand", t."name" as "type"
+        SELECT e."id", e."email", e."firstName", e."zip", b."label" as "brand", t."label" as "type"
           FROM "Employees" e  
           LEFT JOIN "SupportedTypes" st ON e."id" = st."employeeId"
           LEFT JOIN "SupportedBrands" sb ON e."id" = sb."employeeId"
           LEFT JOIN "Brands" b ON b."id" = sb."brandId" 
           LEFT JOIN "Types" t ON t."id" = st."typeId"
         WHERE e."zip" = '${zipcode}'
-        AND b."name" = '${brand}'
-        AND t."name" = '${type}'
+        AND b."value" = '${brand}'
+        AND t."value" = '${type}'
       ;
     `
     const [availableEmployees = []] = await db.connection.query(query)
@@ -45,6 +45,7 @@ export default async (req: Request, res: Response): Promise<Response> => {
 
     return response(req, res, rs[200], sm.ok, result);
   } catch (error) {
+    console.log('================================', error)
     return response(req, res, rs[500], sm.internalServerError, error);
   }
 };
