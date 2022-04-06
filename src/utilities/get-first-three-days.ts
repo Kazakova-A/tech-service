eb984e75fed8aeb20d71f794f327a56567eac104
Arina <arinakazakovak@gmail.com>
2022-04-06 13:54:37 +0300

    fix dates counting

--- src/utilities/get-first-three-days.ts
+++ src/utilities/get-first-three-days.ts
@@ -6,11 +6,10 @@ export default (start: number): {
   thirdDay: number;
   end: number;
 } => {
-  const startDay = moment(start * 1000);
   const firstDay = start;
-  const secondDay = startDay.add(1, 'days').toDate().valueOf() / 1000;
-  const thirdDay = startDay.add(2, 'days').toDate().valueOf() / 1000;
-  const end = startDay.add(3, 'days').toDate().valueOf() / 1000;
+  const secondDay = moment(start * 1000).add(1, 'days').toDate().valueOf() / 1000;
+  const thirdDay = moment(start * 1000).add(2, 'days').toDate().valueOf() / 1000;
+  const end = moment(start * 1000).add(3, 'days').toDate().valueOf() / 1000;
   return {
     firstDay,
     secondDay,
