618101cc26c900165ec8f28ea05f0036f08057f2
alexeyloon <alexeyloon921@gmail.com>
2022-04-25 16:17:31 +0300

    added brand and types, redundant parameters removed:invoiceNumber,description,note,workStatus,onMyWayAt,arrivalWindow,totalAmount,outstandingBalance,e.t.c.

--- src/db/models/jobs.ts
+++ src/db/models/jobs.ts
@@ -1,20 +1,14 @@
 import { Generic, JobStatuses } from '../types';
 
 export interface JobsData extends Generic {
-  invoiceNumber: string;
-  description: string;
   customerId: number;
-  note: string | null;
-  workStatus: JobStatuses;
-  onMyWayAt: number | null;
   startedAt: number | null;
   completedAt: number | null;
-  scheduledStart: number | null;
-  scheduledEnd: number | null;
-  arrivalWindow: number;
-  totalAmount: number;
-  outstandingBalance: number;
   employeeId: number;
+  diagnosticSpentTime: number | null;
+  brand: string;
+  tecTypes: string;
+
 };
 
 export default (
@@ -23,12 +17,6 @@ export default (
 ) => database.define(
   'Jobs',
   {
-    invoiceNumber: {
-      type: DataTypes.STRING,
-    },
-    description: {
-      type: DataTypes.STRING,
-    },
     customerId: {
       allowNull: false,
       onDelete: 'CASCADE',
@@ -39,35 +27,14 @@ export default (
       },
       type: DataTypes.INTEGER,
     },
-    note: {
-      type: DataTypes.STRING,
-    },
-    workStatus: {
-      type: DataTypes.STRING,
-    },
-    onMyWayAt: {
-      type: DataTypes.BIGINT,
-    },
     startedAt: {
       type: DataTypes.BIGINT,
     },
     completedAt: {
       type: DataTypes.BIGINT,
     },
-    scheduledStart: {
+    diagnosticSpentTime: {
       type: DataTypes.BIGINT,
-    },
-    scheduledEnd: {
-      type: DataTypes.BIGINT,
-    },
-    arrivalWindow: {
-      type: DataTypes.INTEGER,
-    },
-    totalAmount: {
-      type: DataTypes.INTEGER,
-    },
-    outstandingBalance: {
-      type: DataTypes.INTEGER,
     },
     employeeId: {
       allowNull: false,
@@ -78,6 +45,12 @@ export default (
         model: 'Employees',
       },
       type: DataTypes.INTEGER,
+    },
+    brand: {
+      type: DataTypes.STRING,
+    },
+    tecTypes: {
+      type: DataTypes.STRING,
     },
     isDeleted: {
       defaultValue: false,
