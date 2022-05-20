4dba632db969f50586361343e5f51b81b7520c80
alexeyloon <alexeyloon921@gmail.com>
2022-05-20 12:53:21 +0300

    add: employees List

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import jobs from './apis/jobs';
 import jobsForEmployees from './apis/jobsForEmployees';
 import employeesAddress from './apis/employeesAddress';
 import employeesDiagnosticCoefficient from './apis/employeesDiagnosticCoefficient';
+import employeesList from './apis/employeesList';
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,6 +21,7 @@ app.use('/jobs', jobs);
 app.use('/jobsForEmployees', jobsForEmployees); //created 20 complited jobs for 3 employees
 app.use('/employeesAddress', employeesAddress);
 app.use('/employeesDiagnosticCoefficient', employeesDiagnosticCoefficient);
+app.use('/employeesList', employeesList);
 
 // start the Express server
 app.listen(PORT, () => {
